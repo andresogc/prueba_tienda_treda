cf089844b7f6bf6dc294fdd387cddb81aafa4414
andresogc <andresogc2011@gmail.com>
2020-10-21 19:59:30 -0500

    Agregar y configurar relacion de modelos Tienda - Producto

---
+++ app/Models/Tienda.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tienda extends Model
+{
+    use HasFactory;
+
+
+    public function productos()
+    {
+        return $this->hasMany('App\Models\Producto');
+    }
+}
